e9a8b1e8185cd092d5a49e0c30c39cca278bddd3
root <root@localhost.localdomain>
2019-04-21 12:25:06 +0800

    ignore logfile, add myblog.xml

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from DjangoUeditor.models import UEditorField  # 头部增加这行代码导入UEditorField
+#from DjangoUeditor.models import UEditorField  # 头部增加这行代码导入UEditorField
 
 
 # 导入Django自带用户模块
