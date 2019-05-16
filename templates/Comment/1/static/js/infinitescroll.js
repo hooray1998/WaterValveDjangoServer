7adf509d62b1a4dee4b691b34d8c792d316bd935
hooray <hooray1998@foxmail.com>
2019-05-16 16:11:44 +0800

    删除了一些没用的图片

--- templates/Comment/1/static/js/infinitescroll.js
+++
@@ -1,9 +0,0 @@
-$(function($){var currentPage=1;var pathname=window.location.pathname;var $document=$(document);var $result=$('.post-feed');var buffer=100;var ticking=false;var isLoading=false;var lastScrollY=window.scrollY;var lastWindowHeight=window.innerHeight;var lastDocumentHeight=$document.height();pathname=pathname.replace(/#(.*)$/g,'').replace('/\//g','/');function onScroll(){lastScrollY=window.scrollY;requestTick();}
-function onResize(){lastWindowHeight=window.innerHeight;lastDocumentHeight=$document.height();requestTick();}
-function requestTick(){if(!ticking){requestAnimationFrame(infiniteScroll)}
-ticking=true;}
-function infiniteScroll(){if(isLoading){return;}
-if(lastScrollY+lastWindowHeight<=lastDocumentHeight-buffer){ticking=false;return;}
-if(currentPage===maxPages){return;}
-isLoading=true;currentPage++;var nextPage=pathname+'page/'+currentPage+'/';$.get(nextPage,function(content){$result.append($(content).find('.post').hide().fadeIn(100));}).fail(function(xhr){if(xhr.status===404){window.removeEventListener('scroll',onScroll,{passive:true});window.removeEventListener('resize',onResize);}}).always(function(){lastDocumentHeight=$document.height();isLoading=false;ticking=false;});}
-window.addEventListener('scroll',onScroll,{passive:true});window.addEventListener('resize',onResize);infiniteScroll();});
