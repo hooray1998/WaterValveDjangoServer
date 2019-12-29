a1761db9d5d31b14f5c83e9970236ceaeb787eca
hooray <hooray1998@foxmail.com>
2019-12-29 13:48:23 +0800

    添加返回数据格式

---
+++ web.js
@@ -0,0 +1,80 @@
+deviceRight:{
+    "res": true,
+    "deviceRight": {
+        "deviceId": 1,
+        "name": "new-name",
+        "serialNum": "nuaa_1",
+        "remarkName": "",
+        "admin": false,
+        "ctrl": true
+    },
+    "deviceConfig": {
+        "admin": "phone_1",
+        "name": "new-name",
+        "remarkName": "",
+        "serialNum": "nuaa_1",
+        "remark": "modify_remark",
+        "accessCtrl": false,
+        "password": "4321",
+        "source": 2,
+        "aAccess": 1,
+        "pAccess": 1,
+        "aPhone": "phone_1"
+    }
+},
+deviceInfo:{
+    "res": true,
+    "deviceInfo": {
+        "position": 99,
+        "ioState": 1,
+        "accuracy": 13,
+        "state": 1,
+        "network": 0,
+        "error": 2
+    }
+},
+deviceConfig:{
+    "res": true,
+    "deviceConfig": {
+        "admin": "phone_1",
+        "name": "new-name",
+        "remarkName": "",
+        "serialNum": "nuaa_1",
+        "remark": "modify_remark",
+        "accessCtrl": false,
+        "password": "4321",
+        "source": 2,
+        "aAccess": 1,
+        "pAccess": 1,
+        "aPhone": "phone_1"
+    }
+},
+deviceList:{
+    "res": true,
+    "deviceList": [
+        {
+            "deviceId": 3,
+            "name": "Name3",
+            "serialNum": "nuaa_3",
+            "remarkName": "newDevice",
+            "admin": true,
+            "ctrl": true
+        },
+        {
+            "deviceId": 1,
+            "name": "new-name",
+            "serialNum": "nuaa_1",
+            "remarkName": "newDevice",
+            "admin": false,
+            "ctrl": true
+        },
+        {
+            "deviceId": 2,
+            "name": "Name2",
+            "serialNum": "nuaa_2",
+            "remarkName": "newDevice",
+            "admin": false,
+            "ctrl": true
+        }
+    ]
+}
